cd9ca800b2fd27f4e010fedc2675e003819a88c8
Rodrigo Bacila <rodrigo.bacila@gmail.com>
2022-03-09 18:08:43 -0300

    Hangfire

---
+++ Hangfire/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hangfire
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection UseHangfire(this IServiceCollection services)
+        {
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(@"Server=.\SQLEXPRESS; Database=Hangfire; Integrated Security=True",
+                    new SqlServerStorageOptions
+                    {
+                        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                        QueuePollInterval = TimeSpan.Zero,
+                        UseRecommendedIsolationLevel = true,
+                        DisableGlobalLocks = true
+                    }));
+
+            services.AddHangfireServer();
+
+            return services;
+        }
+    }
+}
---
+++ Hangfire/FuncJobActivator.cs
@@ -0,0 +1,14 @@
+﻿namespace Hangfire
+{
+    public class FuncJobActivator : JobActivator
+    {
+        private readonly Func<Type, object> resolver;
+
+        public FuncJobActivator(Func<Type, object> resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public override object ActivateJob(Type jobType) => resolver(jobType);
+    }
+}
